5ab4d9d86860ad18d7a857c15446ca65c171e85f
SolTTeucas <122246568+SolTTeucas@users.noreply.github.com>
2024-10-29 22:08:58 +0700

    Update bidangPersegi.go

--- weeks/week-05/bidangPersegi.go
+++ weeks/week-05/bidangPersegi.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var banyak, sisi int64
+
+	fmt.Println("PROGRAM BIDANG PERSEGI")
+	fmt.Println("======================")
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("Masukkan jumlah persegi")
+	fmt.Scan(&banyak)
+	time.Sleep(1 * time.Second)
+	fmt.Println("----------------------")
+	time.Sleep(1 * time.Second)
+	fmt.Printf("\n")
+	time.Sleep(1 * time.Second)
+
+	//berbagai perintah prints untuk menampilkan hasil
+	fmt.Println("RESULT!")
+
+	for i := int64(0); i < banyak; i++ {
+		fmt.Printf("Masukkan panjang sisi untuk persegi %d: ", i+1)
+		fmt.Scan(&sisi) // Meminta input sisi untuk setiap persegi
+
+		// Menghitung keliling dan luas
+		hasilKeliling := 4 * sisi
+		hasilLuas := sisi * sisi
+
+		// Menampilkan hasil
+		fmt.Printf("Persegi %d: Keliling = %d, Luas = %d\n", i+1, hasilKeliling, hasilLuas)
+		time.Sleep(500 * time.Millisecond)
+	}
+
+}
